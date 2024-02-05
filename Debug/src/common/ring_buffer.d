aa35dd83c0ae0a25e8978eee23b9e638b0abc028
WHS <wang8763@naver.com>
2024-02-06 08:39:00 +0900

    Revert "remove ignored file"
    This reverts commit 4cd9d559e492410faaf53da6f901747f8bdc6f54.

--- Debug/src/common/ring_buffer.d
+++
@@ -1,5 +0,0 @@
-src/common/ring_buffer.o: ../src/common/ring_buffer.c \
- ../src/common/ring_buffer.h \
- C:/Users/jjins/Documents/Project/Step_Motor/src/bsp/def.h
-../src/common/ring_buffer.h:
-C:/Users/jjins/Documents/Project/Step_Motor/src/bsp/def.h:
